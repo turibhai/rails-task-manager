06ef2fcdb02eef360932b998aeda19cd747ac8b2
Tsering Lama <tseriing.boksha@gmail.com>
2025-05-15 22:11:06 +0900

    delete working

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -38,7 +38,6 @@ class TasksController < ApplicationController
 
     def destroy
          @task = Task.find(params["id"])
-         raise
          @task.destroy
          redirect_to tasks_path, status: :see_other
         
